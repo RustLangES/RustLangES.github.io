6d2b0eada0ed2e5fc45462a8eda2668398a28363
michaelcardoza <mc.michaelcardoza@gmail.com>
2023-08-25 17:01:29 -0500

    refactor: update home page

--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,5 +1,6 @@
 use leptos::*;
 use crate::components::hero::Hero;
+use crate::components::out_communities::OurCommunities;
 use crate::components::layout::other_communities::OtherCommunities;
 
 #[component]
@@ -7,11 +8,12 @@ pub fn Index(
     cx: Scope
 ) -> impl IntoView {
     view!{ cx,
-        <div>
+        <main>
             <Hero />
+            <OurCommunities />
             <div class="bg-grid-slate-300 dark:bg-grid-slate-900">
                 <OtherCommunities />
             </div>
-        </div>
+        </main>
     }
 }
