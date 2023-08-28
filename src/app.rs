43271daae1c946a1219d57a564fb0cc0fb4fc3f0
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2023-08-28 03:41:22 -0300

    fix: change the way to load some resources

--- src/app.rs
+++ src/app.rs
@@ -9,8 +9,6 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Stylesheet id="leptos" href="/pkg/tailwind.css"/>
-        <Link rel="shortcut icon" type_="image/ico" href="/LogoSegunMichael.ico"/>
         <Router>
             <Routes>
                 <Route path="" view=|| view! { <Index /> }/>
