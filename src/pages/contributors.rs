7a48c046236aff016545cc218b7cce3f6ca980e0
michaelcardoza <mc.michaelcardoza@gmail.com>
2023-09-01 02:03:34 -0500

    refactor: fetch api contributors

--- src/pages/contributors.rs
+++ src/pages/contributors.rs
@@ -1,54 +1,51 @@
-use leptos::*;
+use leptos::{error::Result, *};
+use serde::{Deserialize, Serialize};
 
 use crate::components::{cards::contributor_card::ContributorCard, footer::Footer, header::Header};
 
-struct ContributorItem {
-    name: &'static str,
-    description: &'static str,
-    link: &'static str,
-    brand_src: &'static str,
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Contributor {
+    login: String,
+    avatar_url: String,
+    url: String,
+}
+
+async fn fetch_contributors() -> Result<Vec<Contributor>> {
+    let response = reqwasm::http::Request::get(
+        &"https://api.github.com/repos/RustLangES/rustlanges.github.io/contributors",
+    )
+    .send()
+    .await?
+    .json::<Vec<Contributor>>()
+    .await?
+    .into_iter()
+    .map(|item| item)
+    .collect::<Vec<_>>();
+    Ok(response)
 }
 
 #[component]
 pub fn Contributors() -> impl IntoView {
-    let contributors: Vec<ContributorItem> = vec![
-        ContributorItem {
-            name: "Phosphorus Moscu",
-            description: "Student in Computer Science degree at Universidad Nacional del Oeste | Developer Consultant at Globant",
-            link: "https://github.com/Phosphorus-M",
-            brand_src: "https://avatars.githubusercontent.com/u/19656993?v=4",
-        },
-        ContributorItem {
-            name: "Sergio Alejandro Ribera Costa",
-            description: "Enthusiastic developer Linux and Open Source lover",
-            link: "https://github.com/SergioRibera",
-            brand_src: "https://avatars.githubusercontent.com/u/56278796?v=4",
-        },
-        ContributorItem {
-            name: "Michael Cardoza",
-            description: "Software Developer",
-            link: "https://github.com/michaelcardoza",
-            brand_src: "https://avatars.githubusercontent.com/u/8800455?v=4",
-        },
-        ContributorItem {
-            name: "Emilio Ruscitti",
-            description: "Rust Dev",
-            link: "https://github.com/Lemin-n",
-            brand_src: "https://avatars.githubusercontent.com/u/88170949?v=4",
-        },
-        ContributorItem {
-            name: "carbon based lifeform",
-            description: "Rust Dev",
-            link: "https://github.com/ph4un00b",
-            brand_src: "https://avatars.githubusercontent.com/u/1057021?v=4",
-        },
-        ContributorItem {
-            name: "CrawKatt",
-            description: "Aprendiz de Rust, estudiante de AIEP, Chileno",
-            link: "https://github.com/CrawKatt",
-            brand_src: "https://avatars.githubusercontent.com/u/108593932?v=4",
-        },
-    ];
+    let contributors_results = create_local_resource(move || (), |_| fetch_contributors());
+
+    let contributors_view = move || {
+        let data = contributors_results.read();
+        let Some(Ok(items)) = data else { return None };
+        let result = items
+            .iter()
+            .map(|item| {
+                view! {
+                    <ContributorCard
+                        name=item.login.clone()
+                        description=""
+                        link=item.url.clone()
+                        brand_src=item.avatar_url.clone()
+                    />
+                }
+            })
+            .collect::<Fragment>();
+        Some(result.into_view())
+    };
 
     view! {
         <div>
@@ -61,19 +58,7 @@ pub fn Contributors() -> impl IntoView {
                             <span class="font-alfa-slab text-orange-500">"Colaboradores"</span>
                         </h2>
                         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-6">
-                            {contributors
-                                .into_iter()
-                                .map(|contributor| {
-                                    view! {
-                                        <ContributorCard
-                                            name=contributor.name
-                                            description=contributor.description
-                                            link=contributor.link
-                                            brand_src=contributor.brand_src
-                                        />
-                                    }
-                                })
-                                .collect::<Vec<_>>()}
+                            {contributors_view}
                         </div>
                     </div>
                 </section>
