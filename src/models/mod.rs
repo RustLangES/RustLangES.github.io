2ec279a0c318819f6ae30c50d92b5c058e49ea87
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-09-02 19:02:50 -0400

    feat: separe into models mod

---
+++ src/models/mod.rs
@@ -0,0 +1,18 @@
+pub struct CommunityItem {
+    pub name: &'static [&'static str],
+    pub description: &'static str,
+    pub link: &'static str,
+    pub icon: &'static str,
+    pub brand_src: &'static str,
+    pub brand_alt: &'static str,
+}
+pub struct ProjectItem {
+    pub name: &'static [&'static str],
+    pub description: &'static str,
+    pub link: &'static str,
+    pub brand_src: &'static str,
+    pub button_link: &'static str,
+    pub button_text: &'static str,
+    pub brand_as_letter: bool,
+    pub button_bg_color: &'static str,
+}
