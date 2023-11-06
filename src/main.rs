941fd24f4a51e3b1a7fb56086763a1f8b873d7f2
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2023-11-06 16:42:21 -0300

    feat: send a request to the API to track the previous url

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,14 @@ mod pages;
 
 use app::*;
 use leptos::{logging::log, *};
-use log::info;
+use log::{error, info};
+use web_sys::Url;
+
+static API_URL: &str = "https://rust-lang-en-espanol-api.shuttleapp.rs";
 
 pub fn main() {
+    let _ = create_local_resource(move || (), |_| track_previous_url());
+
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
@@ -20,4 +25,20 @@ pub fn main() {
     mount_to_body(|| {
         view! { <App/> }
     });
+}
+
+pub async fn track_previous_url() {
+    let previous_domain = if document().referrer() == "" {
+        "Undefined".to_owned()
+    } else {
+        let address = document().referrer();
+        let url = Url::new(&address).unwrap();
+        url.host()
+    };
+
+    let endpoint = format!("{API_URL}/track/count?reference={previous_domain}");
+    match reqwasm::http::Request::post(&endpoint).send().await {
+        Ok(_) => log!("Tracking previous url"),
+        Err(_) => error!("Error tracking previous url"),
+    };
 }
