b687da010b9d9d0098ab8fa765dd40169dc66d5e
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2024-01-08 02:56:54 -0300

    perf: remove useless div @Michael

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -10,26 +10,30 @@ pub fn Hero() -> impl IntoView {
     };
 
     view! {
-        <div class="flex items-center justify-center py-14 lg:py-32 px-4">
-            <div class="grid items-center gap-x-20 gap-y-10 lg:grid-cols-2 w-full">
-                <figure class="w-80 mx-auto lg:w-full">
-                    <img src=image_src alt="Rust Lang en Español" width="500" class="ml-auto"/>
-                </figure>
-                <div class="">
-                    <h1 class="flex flex-col mb-4 gap-y-2">
-                        <span class="font-work-sans text-4xl font-light text-center lg:text-left">
-                            "Bienvenidos a"
-                        </span>
-                        <span class="font-alfa-slab text-orange-500 text-6xl sm:text-7xl lg:text-8xl text-center lg:text-left">
-                            "Rust Lang"
-                        </span>
-                        <span class="font-work-sans text-5xl font-semibold text-center lg:text-left">
-                            "En Español"
-                        </span>
-                    </h1>
-                    <SloganButton/>
-                </div>
+        <section class="grid items-center py-14 lg:py-32 px-4 gap-x-20 gap-y-10 lg:grid-cols-2 w-full">
+            <figure class="w-80 mx-auto lg:w-full">
+                <img
+                    src=image_src
+                    alt="Rust Lang en Español"
+                    height="300"
+                    width="500"
+                    class="ml-auto"
+                />
+            </figure>
+            <div>
+                <h1 class="flex flex-col mb-4 gap-y-2">
+                    <span class="font-work-sans text-4xl font-light text-center lg:text-left">
+                        "Bienvenidos a"
+                    </span>
+                    <span class="font-alfa-slab text-orange-500 text-6xl sm:text-7xl lg:text-8xl text-center lg:text-left">
+                        "Rust Lang"
+                    </span>
+                    <span class="font-work-sans text-5xl font-semibold text-center lg:text-left">
+                        "En Español"
+                    </span>
+                </h1>
+                <SloganButton/>
             </div>
-        </div>
+        </section>
     }
 }
