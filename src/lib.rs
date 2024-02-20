391e2eb9fd42e0d2940cc0684d47a226d50cd0ac
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-02-20 18:18:16 -0400

    feat: add bindgen to console log

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,40 @@ pub mod pages;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(msg: String);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn warn(msg: String);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(msg: String);
+}
+
+#[macro_export]
+macro_rules! log {
+    ($($t:tt)*) => {
+        if cfg!(debug_assertions) {
+            log(format_args!($($t)*).to_string())
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($($t:tt)*) => (warn(format_args!($($t)*).to_string()))
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($t:tt)*) => (error(format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
 pub fn hydrate() {
-    console_error_panic_hook::set_once();
+    #[cfg(target_arch = "wasm32")]
+    std::panic::set_hook(Box::new(|info: &std::panic::PanicInfo| error!("{info}")));
 
     leptos::leptos_dom::HydrationCtx::stop_hydrating();
 }
