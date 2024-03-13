de6a07670c4c1161493b6664680bbc8317e1ce37
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2024-03-13 14:34:18 -0300

    feat: add canonical link

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,8 @@
 use leptos::{component, use_context, view, IntoView, SignalGet};
-use leptos_meta::{provide_meta_context, Body, Html, Meta, Stylesheet, Title};
-use leptos_router::{Router, RouterIntegrationContext, Routes, StaticParamsMap, StaticRoute};
+use leptos_meta::{provide_meta_context, Body, Html, Link, Meta, Stylesheet, Title};
+use leptos_router::{
+    use_location, Router, RouterIntegrationContext, Routes, StaticParamsMap, StaticRoute,
+};
 
 use crate::{
     components::{Footer, Header},
@@ -26,38 +28,48 @@ pub fn App() -> impl IntoView {
     };
 
     view! {
-        <Html lang="es"/>
-        <Meta charset="utf-8"/>
-        <Meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <Stylesheet id="fonts" href=format!("{}/fonts.css", assets_folder)/>
-        <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
-        <Title text="Rust Lang en Español"/>
-        <Meta
-            name="description"
-            content="Únete a nuestra comunidad de Rust en español. Promovemos el aprendizaje y la difusión de Rust, compartiendo conocimientos y proyectos emocionantes."
-        />
-
-        <Meta name="og:site_name" content="Rust Lang en Español"/>
-        <Meta name="og:title" content="Bienvenidos a Rust Lang en Español"/>
-        <Meta
-            name="og:description"
-            content="Únete a nuestra comunidad de Rust en español. Promovemos el aprendizaje y la difusión de Rust, compartiendo conocimientos y proyectos emocionantes."
-        />
-        <Meta name="og:url" content="https://rustlang-es.org"/>
-        <Meta name="og:image" content=format!("https://rustlang-es.org/{preview}")/>
-        <Meta name="twitter:image" content=format!("https://rustlang-es.org/{preview}")/>
-        <Meta name="twitter:card" content="summary_large_image"/>
-        <Meta name="twitter:site" content="@rustlang"/>
-        <Meta
-            name="google-site-verification"
-            content="OntIe2SKuQalaapGvxdded9tU4G2p57h0A6e0Rkoni0"
-        />
-
-        <Body class=format!(
-            "bg-orange-200 dark:bg-[#131313]/90 bg-center bg-fixed {} dark:bri dark:bg-cover dark:bg-blend-darken dark:backdrop-blur-xl overflow-x-hidden dark:text-[#e2cea9]",
-            bg_in_dark_mode,
-        )/>
         <Router>
+            <Html lang="es"/>
+            <Meta charset="utf-8"/>
+            <Meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Stylesheet id="fonts" href=format!("{}/fonts.css", assets_folder)/>
+            <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+            <Title text="Rust Lang en Español"/>
+            <Meta
+                name="description"
+                content="Únete a nuestra comunidad de Rust en español. Promovemos el aprendizaje y la difusión de Rust, compartiendo conocimientos y proyectos emocionantes."
+            />
+
+            <Meta name="og:site_name" content="Rust Lang en Español"/>
+            <Meta name="og:title" content="Bienvenidos a Rust Lang en Español"/>
+            <Meta
+                name="og:description"
+                content="Únete a nuestra comunidad de Rust en español. Promovemos el aprendizaje y la difusión de Rust, compartiendo conocimientos y proyectos emocionantes."
+            />
+            <Meta name="og:url" content="https://rustlang-es.org"/>
+            <Meta name="og:image" content=format!("https://rustlang-es.org/{preview}")/>
+            <Meta name="twitter:image" content=format!("https://rustlang-es.org/{preview}")/>
+            <Meta name="twitter:card" content="summary_large_image"/>
+            <Meta name="twitter:site" content="@rustlang"/>
+            <Meta
+                name="google-site-verification"
+                content="OntIe2SKuQalaapGvxdded9tU4G2p57h0A6e0Rkoni0"
+            />
+
+            {
+                let location = use_location().pathname;
+                view! {
+                    <Link
+                        rel="canonical"
+                        href=format!("https://www.rustlang-es.org{}", location.get().as_str())
+                    />
+                }
+            }
+
+            <Body class=format!(
+                "bg-orange-200 dark:bg-[#131313]/90 bg-center bg-fixed {} dark:bri dark:bg-cover dark:bg-blend-darken dark:backdrop-blur-xl overflow-x-hidden dark:text-[#e2cea9]",
+                bg_in_dark_mode,
+            )/>
             <Header/>
             <main>
                 <Routes>
