6314d2edf6ed43aac1a702dd03ca535386c37aa9
Sergio Alejandro Ribera Costa <56278796+SergioRibera@users.noreply.github.com>
2024-12-29 02:56:16 -0400

    Update src/components/icons/discord_icon.rs
    Co-authored-by: Phosphorus Moscu <fernando.n.p.d.moscu@gmail.com>

--- src/components/icons/discord_icon.rs
+++ src/components/icons/discord_icon.rs
@@ -3,7 +3,7 @@ use leptos::{component, view, IntoView};
 #[component]
 pub fn DiscordIcon(
     #[prop(default = 40)] size: u32,
-    #[prop(default = "fill-black dark:fill-[#bf8821]")] class: &'static str,
+    #[prop(default = "fill-current dark:fill-[#bf8821]")] class: &'static str,
 ) -> impl IntoView {
     view! {
         <svg
